5cb46e17534f9f83185207d5bc70336f899c30d4
fajar cahyadi putra <fajarcahyadiputra@gmail.com>
2020-12-21 14:28:21 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const flash = require('connect-flash');
 
 //import database
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/staycation',{
+mongoose.connect('mongodb+srv://fajarcahyadiputra:12Temansejati@cluster0.frea1.mongodb.net/db_staycation?retryWrites=true&w=majority',{
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
